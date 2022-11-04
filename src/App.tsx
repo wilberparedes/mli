737c754165a4d5cc1ccc3ee475fae42ef55615e4
Wilber Paredes <wilberparedes@gmail.com>
2022-11-04 00:14:22 -0500

    feat: init router in app and config globalstyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { FC } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { Dashboard } from './modules/dashboard/dashboard.component'
+import { GlobalStyle } from './styles/global.style'
+export const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <GlobalStyle />
+      <Dashboard />
+    </BrowserRouter>
+  )
 }
-
-export default App;
---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  primary: '#FFE600',
+  black: '#333333',
+  grayDark: '#666666',
+  gray: '#999999',
+  grayLight: '#EEEEEE',
+  blue: '#3083FD',
+}
